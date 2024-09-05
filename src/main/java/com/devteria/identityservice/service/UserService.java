4c3e6ec16d557342a061915b28711f77ac95fb45
BoLaiCodeDao <anhdtit03@gmail.com>
2024-09-05 20:01:35 +0700

    Done authentication

--- src/main/java/com/devteria/identityservice/service/UserService.java
+++ src/main/java/com/devteria/identityservice/service/UserService.java
@@ -12,15 +12,21 @@ import com.devteria.identityservice.repository.UserRepository;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class UserService {
     UserRepository userRepository;
@@ -42,6 +48,16 @@ public class UserService {
         return userMapper.toUserResponse(userRepository.save(user));
     }
 
+    public UserResponse getMyInfo() {
+       var context = SecurityContextHolder.getContext();
+       String name = context.getAuthentication().getName();
+
+       User user = userRepository.findByUsername(name).orElseThrow(
+               () -> new AppException(ErrorCode.USER_NOT_EXISTED));
+
+       return userMapper.toUserResponse(user);
+    }
+
     public UserResponse updateUser(String userId, UserUpdateRequest request) {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new RuntimeException("User not found"));
@@ -55,12 +71,16 @@ public class UserService {
         userRepository.deleteById(userId);
     }
 
+    @PreAuthorize("hasRole('ADMIN')") //kiểm tra role trước khi vào method
     public List<UserResponse> getUsers(){
         return userRepository.findAll().stream()
                 .map(userMapper::toUserResponse).toList();
     }
 
+//    @PostAuthorize("hasRole('ADMIN')") sau khi method chạy xong mới kiểm tra role
+    @PostAuthorize("returnObject.username == authentication.name")
     public UserResponse getUser(String id){
+        log.info("Get id roi ne!!!");
         return userMapper.toUserResponse(userRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("User not found")));
     }
