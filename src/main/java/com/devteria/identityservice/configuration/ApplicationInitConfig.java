b89f96c5763e0cd159ab850559f3f1e6333a4739
BoLaiCodeDao <anhdtit03@gmail.com>
2024-09-05 16:55:49 +0700

    Auth 50$

---
+++ src/main/java/com/devteria/identityservice/configuration/ApplicationInitConfig.java
@@ -0,0 +1,47 @@
+package com.devteria.identityservice.configuration;
+
+import com.devteria.identityservice.entity.User;
+import com.devteria.identityservice.enums.Role;
+import com.devteria.identityservice.repository.UserRepository;
+import jakarta.persistence.Access;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+
+@Configuration
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class ApplicationInitConfig {
+
+    private static final Logger log = LoggerFactory.getLogger(ApplicationInitConfig.class);
+    PasswordEncoder passwordEncoder;
+//tao admin moi khi start prj
+    @Bean
+    ApplicationRunner applicationRunner(UserRepository userRepository) {
+        return args -> {
+            if (userRepository.findByUsername("admin").isEmpty()) {
+                var roles = new HashSet<String>();
+                roles.add(Role.ADMIN.name());
+                User user = User.builder()
+                        .username("admin")
+                        .password(passwordEncoder.encode("Anh05590096867@"))
+                        .roles(roles)
+                        .build();
+
+                userRepository.save(user);
+                log.warn("Admin user has been created with name admin");
+            }
+
+        };
+    }
+}
+
